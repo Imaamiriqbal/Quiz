ca6b4935959e58cbb90f67597c0c45efecb9c401
pc <aamiriqbalamir55@gmail.com>
2022-04-02 11:19:02 +0500

    Login Form

---
+++ app/src/main/java/com/example/pk/edu/uiitbasit_aamir_mohsin/quiz/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pk.edu.uiitbasit_aamir_mohsin.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
